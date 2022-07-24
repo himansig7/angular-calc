bf7dcecbcac3a618b8c45c8b3ac7caf4961e94a3
himansig7 <himansig7@gmail.com>
2022-07-24 14:10:42 -0700

    1 file modified, 3 added

---
+++ src/app/my-calc/my-calc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-calc',
+  templateUrl: './my-calc.component.html',
+  styleUrls: ['./my-calc.component.css']
+})
+export class MyCalcComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
